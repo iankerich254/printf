c2b0d05dd62bae2db7d3e059df37bdbb42b3d493
iankerich254 <ankerich254@gmail.com>
2023-10-17 08:56:49 -0700

    Produce output according to a format given, Task 0

--- _printf.c
+++ _printf.c
@@ -4,7 +4,7 @@
  * @format: identifier to look for.
  * Return: the length of the string.
  */
-int _printf(const char * const format, ...)
+int _printf(const char *format, ...)
 {
 	conv_m m[] = {
 		{"%s", printf_s},
